81c5328bb534ac75a9f8e39345948052d5086a1c
Adebless <sorinolab@yahoo.com>
2023-06-19 04:45:51 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
  * main - A program that prints the size of various computer types
- * Return: Always 0 (Sucess)
+ * Return: 0 (Sucess)
  */
 int main(void)
 {
-	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	fputs("and that piece of art is useful\"- Dora Korpar, 2015-10-19\n", stdout);
 	return (1);
 }
