2916494c97e230fa1ace12c82524431c17bbd3cd
Adebless <sorinolab@yahoo.com>
2023-06-22 17:27:20 -0700

    3-print_remaining_days.c

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -9,10 +9,9 @@
 
 void print_remaining_days(int month, int day, int year)
 {
-
-	if ((year % 4 == 0) &&n(year % 400 == 0 || && year % 100 != 0))
+	if ((year % 100 == 0 && year % 400 == 0) || (year % 4 == 0))
 	{
-	if (month >= 3 && day >= 60)
+	if (month > 2 && day >= 60)
 	{
 	day++;
 	}
@@ -29,6 +28,6 @@ void print_remaining_days(int month, int day, int year)
 	{
 	printf("Day of the year: %d\n", day);
 	printf("Remaining days: %d\n", 365 - day);
-	}
-	}
+}
+}
 }
